8420f570542f3832deede132aaa88ff8d6d531ec
Scott Laing <fifersheep@gmail.com>
2020-01-23 00:00:01 +0000

    parameterise navigation drawer widget test for each route

--- test/widgets/navigation_drawer_test.dart
+++ test/widgets/navigation_drawer_test.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:portfolio/widgets/blog/blog_page.dart';
+import 'package:portfolio/widgets/experience/experience_page.dart';
+import 'package:portfolio/widgets/intro/intro_page.dart';
 import 'package:portfolio/widgets/navigation/navigation_drawer.dart';
 import 'package:portfolio/widgets/navigation/navigation_routes.dart';
+import 'package:portfolio/widgets/projects/projects_page.dart';
 
 class MockNavigatorObserver extends Mock implements NavigatorObserver {}
 
@@ -14,30 +18,37 @@ void main() {
       navigatorObserver = MockNavigatorObserver();
     });
 
-    testWidgets('projects navigation item navigates to the projects page',
-        (tester) async {
-      await tester.pumpWidget(MaterialApp(
-        navigatorObservers: [navigatorObserver],
-        routes: routes,
-        home: Scaffold(
-          appBar: AppBar(),
-          drawer: NavigationDrawer(),
-        ),
-      ));
+    var routingTestParams = {
+      "Intro": IntroPage,
+      "Projects": ProjectsPage,
+      "Experience": ExperiencePage,
+      "Blog": BlogPage
+    };
+    routingTestParams.forEach((String key, Type widgetType) {
+      testWidgets('$key navigation item navigates to the projects page',
+          (tester) async {
+        await tester.pumpWidget(MaterialApp(
+          navigatorObservers: [navigatorObserver],
+          routes: routes,
+          home: Scaffold(
+            appBar: AppBar(),
+            drawer: NavigationDrawer(),
+          ),
+        ));
 
-      var locateDrawer = find.byTooltip('Open navigation menu');
-      await tester.tap(locateDrawer);
-      await tester.pumpAndSettle();
+        var locateDrawer = find.byTooltip('Open navigation menu');
+        await tester.tap(locateDrawer);
+        await tester.pumpAndSettle();
 
-      var navItem = find.byKey(Key('Projects'));
+        var navItem = find.byKey(Key(key));
+        expect(navItem, findsOneWidget);
 
-      expect(navItem, findsOneWidget);
+        await tester.tap(navItem);
+        await tester.pumpAndSettle();
 
-      await tester.tap(navItem);
-      await tester.pumpAndSettle();
-
-      verify(navigatorObserver.didPush(any, any));
-      expect(find.text("Projects Title"), findsOneWidget);
+        verify(navigatorObserver.didPush(any, any));
+        expect(find.byType(widgetType), findsOneWidget);
+      });
     });
   });
 }
