3db11c121fc389079c80660daafd4f6073f4935c
Scott Laing <fifersheep@gmail.com>
2021-03-05 23:07:49 +0000

    path url strategy - goodbye hash

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:portfolio/injection.dart';
 import 'package:portfolio/presentation/constants/images.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'presentation/app.dart';
 import 'presentation/constants/strings.dart';
 
 void main() {
   configureDependencies(Environment.prod);
+  setPathUrlStrategy();
   runApp(Images(child: Strings(child: App())));
 }
