77308714f948f8b8ff3ada13f01b3489c3345102
Scott Laing <fifersheep@gmail.com>
2021-05-10 23:26:55 +0100

    show click hand when hovering over social icons

--- lib/presentation/core/navigation/social_bar.dart
+++ lib/presentation/core/navigation/social_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:portfolio/presentation/constants/colors.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -45,6 +46,7 @@ class _SocialIconState extends State<SocialIcon> {
   Widget build(BuildContext context) => Padding(
         padding: const EdgeInsets.only(right: 16),
         child: MouseRegion(
+          cursor: SystemMouseCursors.click,
           onEnter: (event) {
             setState(() {
               _size = 26;
