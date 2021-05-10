844fdc967856deea66a11a2d4c1579cd789956d6
Scott Laing <fifersheep@gmail.com>
2021-05-10 23:20:59 +0100

    animate social icons on hover

--- lib/presentation/core/navigation/social_bar.dart
+++ lib/presentation/core/navigation/social_bar.dart
@@ -7,30 +7,66 @@ class SocialBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Padding(
         padding: const EdgeInsets.only(left: 32.0),
-        child: Row(
-          children: const [
-            SocialIcon(FontAwesomeIcons.gitlab, "https://gitlab.com/fifersheep"),
-            SocialIcon(FontAwesomeIcons.twitter, "https://twitter.com/fifersheep"),
-            SocialIcon(FontAwesomeIcons.linkedin, "https://linkedin.com/in/scott-laing-edi"),
-          ],
+        child: SizedBox(
+          height: 32,
+          child: Row(
+            children: const [
+              SocialIcon(FontAwesomeIcons.gitlab, "https://gitlab.com/fifersheep"),
+              SocialIcon(FontAwesomeIcons.twitter, "https://twitter.com/fifersheep"),
+              SocialIcon(FontAwesomeIcons.linkedin, "https://linkedin.com/in/scott-laing-edi"),
+            ],
+          ),
         ),
       );
 }
 
-class SocialIcon extends StatelessWidget {
+class SocialIcon extends StatefulWidget {
   const SocialIcon(this.iconData, this.url);
 
   final IconData iconData;
   final String url;
 
   @override
+  _SocialIconState createState() => _SocialIconState();
+}
+
+class _SocialIconState extends State<SocialIcon> {
+  final double _originalSize = 22;
+
+  late double _size;
+
+  @override
+  void initState() {
+    _size = _originalSize;
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) => Padding(
-        padding: const EdgeInsets.only(top: 16, right: 16),
-        child: GestureDetector(
-          onTap: () => launch(url),
-          child: Icon(
-            iconData,
-            color: ThemeColors.primary[400],
+        padding: const EdgeInsets.only(right: 16),
+        child: MouseRegion(
+          onEnter: (event) {
+            setState(() {
+              _size = 26;
+            });
+          },
+          onExit: (event) {
+            setState(() {
+              _size = _originalSize;
+            });
+          },
+          child: GestureDetector(
+            onTap: () => launch(widget.url),
+            child: TweenAnimationBuilder(
+              duration: const Duration(milliseconds: 100),
+              tween: Tween<double>(begin: _originalSize, end: _size),
+              curve: Curves.easeInOut,
+              builder: (_, double size, __) => Icon(
+                widget.iconData,
+                size: size,
+                color: ThemeColors.primary[400],
+              ),
+            ),
           ),
         ),
       );
