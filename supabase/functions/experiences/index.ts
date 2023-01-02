34bfde54d68f9f7030f3ca1f4d72b537c4848003
Scott Laing <fifersheep@gmail.com>
2023-01-02 17:28:48 +0000

    add get experiences edge function

---
+++ supabase/functions/experiences/index.ts
@@ -0,0 +1,60 @@
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+import {
+  createClient,
+  SupabaseClient,
+} from "https://esm.sh/@supabase/supabase-js@2";
+import { corsHeaders } from "../_shared/cors.ts";
+
+serve(async (req) => {
+  const { url, method } = req;
+
+  if (method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
+
+  try {
+    const supabaseClient = createClient(
+      Deno.env.get("SUPABASE_URL") ?? "",
+      Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+      {
+        global: {
+          headers: { Authorization: req.headers.get("Authorization")! },
+        },
+      }
+    );
+
+    const taskPattern = new URLPattern({ pathname: "/experiences" });
+    const matchingPath = taskPattern.exec(url);
+
+    if (method === "GET") {
+      const { data: task, error } = await supabaseClient
+        .from("experiences")
+        .select("*");
+
+      if (error) throw error;
+
+      return new Response(JSON.stringify({ task }), {
+        headers: { ...corsHeaders, "Content-Type": "application/json" },
+        status: 200,
+      });
+    } else {
+      return new Response(JSON.stringify({ error: "Method not in use" }), {
+        headers: { ...corsHeaders, "Content-Type": "application/json" },
+        status: 404,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 400,
+    });
+  }
+});
+
+// To invoke:
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/experiences' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"name":"Functions"}'
