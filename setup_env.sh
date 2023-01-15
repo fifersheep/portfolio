9565c054107c71ee54f8f580cfece68e6e2b2e23
Scott Laing <fifersheep@gmail.com>
2023-01-15 21:59:38 +0000

    rewrite base url attempt 1

--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo SUPABASE_URL=$SUPABASE_URL >> .env
 echo SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY >> .env
+echo -e "<script>(function () {document.write('<base href=\"' + document.location.pathname + '\" />');}());</script>\n$(cat build/web/index.html)" > build/web/index.html
