e075283a014be46351d665a527ebe9b086a5ad10
Scott Laing <fifersheep@gmail.com>
2023-01-25 22:57:14 +0000

    clone flutter repo if necessary on deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+if [ ! -d "flutter" ] then
+    git clone https://github.com/flutter/flutter.git
+fi
+
+cd flutter && git checkout v3.3.9 && cd ..
 flutter packages get
 flutter build web --dart-define=SUPABASE_URL=$SUPABASE_URL --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
