561a54477c3879c8a0685c693b899e993f45f74d
Scott Laing <fifersheep@gmail.com>
2023-01-25 23:06:05 +0000

    fix if block and flutter setup in deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-if [ ! -d "flutter" ] then
+if [ ! -d "flutter" ]; then
     git clone https://github.com/flutter/flutter.git
 fi
 
 cd flutter && git checkout v3.3.9 && cd ..
+export PATH="$PATH:`pwd`/flutter/bin"
+
 flutter packages get
 flutter build web --dart-define=SUPABASE_URL=$SUPABASE_URL --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
