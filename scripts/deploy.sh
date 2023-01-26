bfa36f4c1fc81d39f351fb8b85833786ac5d3192
Scott Laing <fifersheep@gmail.com>
2023-01-26 00:27:56 +0000

    try raising min sdk version to 2.17.0

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
-
-flutter_version=3.7.0
-
 if [ ! -d "flutter" ]; then
     git clone https://github.com/flutter/flutter.git
 fi
 
-
 cd flutter
-git checkout $flutter_version
+git checkout 3.7.0
 cd ..
 export PATH="$PATH:`pwd`/flutter/bin"
 
-contains_tag=$(git describe --tags | grep $flutter_version)
-if [ ! contains_tag ]; then
-   rm -rf flutter/bin/cache
-fi
-
-flutter pub get
+flutter packages get
 flutter build web --dart-define=SUPABASE_URL=$SUPABASE_URL --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
