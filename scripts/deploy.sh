b78f53d40e3d682df1eec05fc5aeefcaaafe21b6
Scott Laing <fifersheep@gmail.com>
2023-01-26 00:12:08 +0000

    remove flutter cache if version changed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+
+flutter_version=3.7.0
+
 if [ ! -d "flutter" ]; then
     git clone https://github.com/flutter/flutter.git
 fi
 
+
 cd flutter
-git checkout 3.7.0
+git checkout $flutter_version
 cd ..
 export PATH="$PATH:`pwd`/flutter/bin"
 
-flutter clean
-flutter packages get
+contains_tag=$(git describe --tags | grep $flutter_version)
+if [ ! contains_tag ]; then
+   rm -rf flutter/bin/cache
+fi
+
+flutter pub get
 flutter build web --dart-define=SUPABASE_URL=$SUPABASE_URL --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
