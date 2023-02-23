5f9b3b03a23128d6a4092a2f7b0048c580f5dae5
Scott Laing <fifersheep@gmail.com>
2023-02-23 00:17:24 +0000

    use shell route for scaffold

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,15 +1,35 @@
-import 'package:flutter/material.dart' hide Router;
+import 'package:flutter/material.dart' hide Router, NavigationDrawer;
 import 'package:go_router/go_router.dart';
 import 'package:portfolio/injection.dart';
 import 'package:portfolio/presentation/constants/strings.dart';
 import 'package:portfolio/presentation/constants/theme.dart';
+import 'package:portfolio/presentation/core/navigation/navigation_drawer.dart';
 import 'package:portfolio/presentation/core/navigation/navigation_route_observer.dart';
 import 'package:portfolio/presentation/routes/routes.dart';
 
 class App extends StatelessWidget {
   final _router = GoRouter(
     initialLocation: Routes.intro,
-    routes: routes,
+    routes: [
+      ShellRoute(
+        routes: routes,
+        builder: (context, state, child) {
+          final bool isMobile = MediaQuery.of(context).size.width < 1000;
+
+          return Row(
+            children: <Widget>[
+              if (!isMobile) const NavigationDrawer(),
+              Expanded(
+                child: Scaffold(
+                  drawer: isMobile ? const NavigationDrawer() : null,
+                  body: child,
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    ],
     observers: [getIt<NavigationRouteObserver>()],
   );
 
